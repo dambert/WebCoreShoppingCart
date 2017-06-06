6b6c35492a7ef724d597b8e5fe012ebd99f4a981
dambert <dmsantillana2705@gmail.com>
2017-06-05 23:35:56 -0500

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace arqui.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
